cfa899a65e336494ba4a268ada643cf81c0836b8
IndraP24 <indrashis985@gmail.com>
2021-07-28 20:05:00 +0530

    Add dashborad streamlit app

---
+++ dashborad.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.markdown("## KPI First Row")
+
+# kpi 1
+
+kpi1, kpi2, kpi3 = st.beta_columns(3)
+
+with kpi1:
+    st.markdown("**First KPI**")
+    num1 = 111
+    st.markdown(
+        f"<h1 style='text-align: center; color: red;'>{num1}</h1>", unsafe_allow_html=True)
