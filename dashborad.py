6e79c51f1fdff7b33b16d4ee596f05c6a60298ff
IndraP24 <indrashis985@gmail.com>
2021-07-28 22:21:54 +0530

    Update UI

--- dashborad.py
+++ dashborad.py
@@ -2,12 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
 st.set_page_config(page_title='Streamlit Dashboard',
                    layout='wide',
                    page_icon='💹')
 
+st.markdown("<h1 style='text-align: center; color: pink;'>COVID19-India Data Visualization</h1>",
+            unsafe_allow_html=True)
 
-st.markdown("<h1 style='text-align: center;'>Main KPIs</h1>",
+st.markdown("<h2 style='text-align: center;'>Main KPIs</h2>",
             unsafe_allow_html=True)
 
 # kpi 1
@@ -18,25 +21,25 @@ with kpi1:
     st.markdown("**First KPI**")
     num1 = 111
     st.markdown(
-        f"<h1 style='text-align: center; color: red;'>{num1}</h1>", unsafe_allow_html=True)
+        f"<h2 style='text-align: center; color: red;'>{num1}</h2>", unsafe_allow_html=True)
 
 with kpi2:
     st.markdown("**Second KPI**")
     num2 = 222
     st.markdown(
-        f"<h1 style='text-align: center; color: red;'>{num2}</h1>", unsafe_allow_html=True)
+        f"<h2 style='text-align: center; color: red;'>{num2}</h2>", unsafe_allow_html=True)
 
 with kpi3:
     st.markdown("**Third KPI**")
     num3 = 333
     st.markdown(
-        f"<h1 style='text-align: center; color: red;'>{num3}</h1>", unsafe_allow_html=True)
+        f"<h2 style='text-align: center; color: red;'>{num3}</h2>", unsafe_allow_html=True)
 
 st.markdown("---")
 
 # second row
 
-st.markdown("<h1 style='text-align: center;'>Secondary KPIs</h1>",
+st.markdown("<h2 style='text-align: center;'>Secondary KPIs</h2>",
             unsafe_allow_html=True)
 
 first_kpi, second_kpi, third_kpi, fourth_kpi, fifth_kpi, sixth_kpi = st.beta_columns(
@@ -47,37 +50,37 @@ with first_kpi:
     st.markdown("**First KPI**")
     number1 = 111
     st.markdown(
-        f"<h1 style='text-align: center; color: yellow;'>{number1}</h1>", unsafe_allow_html=True)
+        f"<h2 style='text-align: center; color: yellow;'>{number1}</h2>", unsafe_allow_html=True)
 
 with second_kpi:
     st.markdown("**Second KPI**")
     number2 = 222
     st.markdown(
-        f"<h1 style='text-align: center; color: yellow;'>{number2}</h1>", unsafe_allow_html=True)
+        f"<h2 style='text-align: center; color: yellow;'>{number2}</h2>", unsafe_allow_html=True)
 
 with third_kpi:
     st.markdown("**Third KPI**")
     number3 = 333
     st.markdown(
-        f"<h1 style='text-align: center; color: yellow;'>{number3}</h1>", unsafe_allow_html=True)
+        f"<h2 style='text-align: center; color: yellow;'>{number3}</h2>", unsafe_allow_html=True)
 
 with fourth_kpi:
     st.markdown("**First KPI**")
     number1 = 111
     st.markdown(
-        f"<h1 style='text-align: center; color: yellow;'>{number1}</h1>", unsafe_allow_html=True)
+        f"<h2 style='text-align: center; color: yellow;'>{number1}</h2>", unsafe_allow_html=True)
 
 with fifth_kpi:
     st.markdown("**Second KPI**")
     number2 = 222
     st.markdown(
-        f"<h1 style='text-align: center; color: yellow;'>{number2}</h1>", unsafe_allow_html=True)
+        f"<h2 style='text-align: center; color: yellow;'>{number2}</h2>", unsafe_allow_html=True)
 
 with sixth_kpi:
     st.markdown("**Third KPI**")
     number3 = 333
     st.markdown(
-        f"<h1 style='text-align: center; color: yellow;'>{number3}</h1>", unsafe_allow_html=True)
+        f"<h2 style='text-align: center; color: yellow;'>{number3}</h2>", unsafe_allow_html=True)
 
 st.markdown("---")
 
