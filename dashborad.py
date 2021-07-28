5f770c212b0a77cfcfc2d6a235d4595ebf914a0f
IndraP24 <indrashis985@gmail.com>
2021-07-28 20:07:14 +0530

    Add first row kpis

--- dashborad.py
+++ dashborad.py
@@ -12,3 +12,15 @@ with kpi1:
     num1 = 111
     st.markdown(
         f"<h1 style='text-align: center; color: red;'>{num1}</h1>", unsafe_allow_html=True)
+
+with kpi2:
+    st.markdown("**First KPI**")
+    num2 = 222
+    st.markdown(
+        f"<h1 style='text-align: center; color: red;'>{num2}</h1>", unsafe_allow_html=True)
+
+with kpi3:
+    st.markdown("**First KPI**")
+    num3 = 333
+    st.markdown(
+        f"<h1 style='text-align: center; color: red;'>{num3}</h1>", unsafe_allow_html=True)
